9309d2898ff993c5f80d708dd264b92bd02daf4d
ammaratahirr35 <ammaratahirr35@gmail.com>
2026-05-07 22:40:44 +0500

    initial commit

---
+++ android/app/src/main/kotlin/com/example/adminpanellostandfound/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.adminpanellostandfound
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/ui/screens/splash_screen.dart
@@ -0,0 +1,98 @@
+import 'dart:async';
+// import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+// import 'package:lost_and_found/ui/screens/BottomNavScreen.dart';
+// import 'package:lost_and_found/ui/screens/login_screen.dart';
+// import 'package:lost_and_found/ui/screens/homepagescreen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false, // Remove debug banner
+      home: SplashScreen(),
+    ),
+  );
+}
+
+class SplashScreen extends StatefulWidget {
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+
+
+
+  @override
+  void initState() {
+    super.initState();
+    // final auth  = FirebaseAuth.instance;
+    // final user = auth.currentUser;
+    // if(user != null){
+    //   Timer(Duration(seconds: 2), () {
+    //     Navigator.pushReplacement(
+    //       context,
+    //       MaterialPageRoute(builder: (context) => BottomNavScreen()
+    //       ),
+    //     );
+    //   });
+    //
+    // }
+    // else
+    // {
+    //   Timer(Duration(seconds: 2), () {
+    //     Navigator.pushReplacement(
+    //       context,
+    //       MaterialPageRoute(builder: (context) => LoginScreen()
+    //       ),
+    //     );
+    //   });
+    // }
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        // color: Color(0xFFD6E2F3),
+        color: Color(0xFFEFF5FB),
+
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "LOST",
+                style: TextStyle(
+                  color: Color(0xFF0D2C54), // Dark blue from logo
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2,
+                ),
+              ),
+              Text(
+                "&",
+                style: TextStyle(
+                  color: Color(0xFFE99A25), // Orange from logo
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                "FOUND",
+                style: TextStyle(
+                  color: Color(0xFF0D2C54), // Dark blue from logo
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
